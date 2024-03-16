71a2141e5dad18ea7a158488729bdb449b1953d1
Ania <aniakbennett@gmail.com>
2024-03-16 19:43:38 -0400

    fixing media query styling issue

--- src/App.vue
+++ src/App.vue
@@ -69,21 +69,4 @@ nav a:hover {
   margin: 0px auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0px 2rem 0px 0px;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
