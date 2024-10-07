4a152b1ffa0c61bc84ef3544f51d3ad525759f42
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-10-07 13:42:16 -0400

    Progress

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import path from 'node:path';
+
+import pluginJs from '@eslint/js';
+import tjwBase from 'eslint-config-tjw-base';
+// import tjwImport from 'eslint-config-tjw-import';
+import tjwVue from 'eslint-config-tjw-vue';
+import pluginImport from 'eslint-plugin-import';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVueA11y from 'eslint-plugin-vuejs-accessibility';
+// import globals from 'globals';
+
+const __dirname = import.meta.dirname;
+const vue3Recommended = pluginVue.configs['flat/recommended'];
+const vueA11yRecommended = pluginVueA11y.configs['flat/recommended'];
+
+export default [
+  pluginJs.configs.recommended,
+  pluginImport.flatConfigs.recommended,
+  ...vue3Recommended,
+  ...vueA11yRecommended,
+  tjwBase,
+  // tjwImport,
+  tjwVue,
+  {
+  //   files: [
+  //     '**/*.{js,mjs,cjs,vue}'
+  //   ],
+  //   ignores: [
+  //     '/dist/'
+  //   ],
+  //   languageOptions: {
+  //     globals: globals.browser
+  //   },
+  //   rules: {
+  //     'import/no-anonymous-default-export': 'off',
+  //     'import/no-namespace': 'off',
+  //     'import/no-unresolved': 'off',
+  //     'vue/no-multiple-template-root': [
+  //       'off'
+  //     ],
+  //     'vuejs-accessibility/label-has-for': [
+  //       'error',
+  //       {
+  //         components: ['Label'],
+  //         required: {
+  //           some: ['nesting', 'id']
+  //         },
+  //         allowChildren: false
+  //       }
+  //     ]
+  //   },
+    settings: {
+      'import/resolver': {
+        vite: {
+          viteConfig: {
+            resolve: {
+              alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@@': path.resolve(__dirname, 'tests'),
+                '@@@': path.resolve(__dirname, 'docs')
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+];
