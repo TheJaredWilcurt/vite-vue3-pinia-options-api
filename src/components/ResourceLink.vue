ed6ecc1977bd119eb0ecef5fc2fbff66459e7b01
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-05-25 19:42:34 -0400

    Abstract link component

---
+++ src/components/ResourceLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a
+    v-text="text"
+    :href="link"
+    target="_blank"
+    rel="noopener"
+  ></a>
+</template>
+
+<script>
+export default {
+  name: 'ResourceLink',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
